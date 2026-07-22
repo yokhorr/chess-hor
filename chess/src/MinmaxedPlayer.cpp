db4a520bae06e9234bc0bafa878458a3e6d6d1ec
Egor Solyanik <yokhor06@mail.ru>
2026-07-22 20:05:32 +1000

    Add alfa-beta for faster bot moves

--- chess/src/MinmaxedPlayer.cpp
+++ chess/src/MinmaxedPlayer.cpp
@@ -16,29 +16,16 @@ Move MinmaxedPlayer::makeMove(Board board) {
 	int32_t minMaxValue = INF;
 	Move bestMove;
 
-	if (const auto currFen = board.exportToFEN(); !table_.contains(currFen)) {
-		for (const auto& move : board.getAllLegalMoves(getColor())) {
-			board.makeMove(move);
-			table_[currFen].emplace_back(board.exportToFEN(), move);
-			if (
-				const int32_t maxEval = evaluatePosition(board, oppositeColor(getColor()), 1);
-				maxEval < minMaxValue
-			) {
-				minMaxValue = maxEval;
-				bestMove = move;
-			}
-			board.undoMove(move);
+	for (const auto& move : board.getAllLegalMoves(getColor())) {
+		board.makeMove(move);
+		if (
+			const int32_t maxEval = evaluatePosition(board, oppositeColor(getColor()), 1, -INF, +INF);
+			maxEval < minMaxValue
+		) {
+			minMaxValue = maxEval;
+			bestMove = move;
 		}
-	} else {
-		for (const auto& [fen, move] : table_.at(currFen)) {
-			if (
-				const int32_t maxEval = evaluatePosition(Board(fen), oppositeColor(getColor()), 1);
-				maxEval < minMaxValue
-			) {
-				minMaxValue = maxEval;
-				bestMove = move;
-			}
-		}
+		board.undoMove(move);
 	}
 
 	return bestMove;
@@ -59,65 +46,44 @@ int32_t MinmaxedPlayer::countMobilityValue(Board const& board, Color const& colo
 }
 
 int32_t MinmaxedPlayer::heuristicValue(Board const& board, Color const& color) {
-	const auto fen = board.exportToFEN();
-	if (positionValuesCache_.contains(fen)) {
-		// std::cout << positionValuesCache_.size() << std::endl;
-		return positionValuesCache_.at(fen);
-	}
-
 	const auto gameOutcome = board.getGameOutcome();
 
 	if (gameOutcome == Outcome::mate) {
-		positionValuesCache_[fen] = -WIN;
 		return -WIN;
 	}
 
 	if (gameOutcome == Outcome::draw) {
 		// NOTE: aggressive: >=, non-aggressive: >
-		if (countMaterialValue(board, color) >= countMaterialValue(board, oppositeColor(color))) {
-			positionValuesCache_[fen] = -WIN;
+		if (countMaterialValue(board, color) > countMaterialValue(board, oppositeColor(color))) {
 			return -WIN;
 		}
-		positionValuesCache_[fen] = WIN;
 		return WIN;
 	}
 
 	const int32_t materialValue = 1000 *
 		(countMaterialValue(board, color) - countMaterialValue(board, oppositeColor(color)));
 	const int32_t mobilityValue = countMobilityValue(board, color) - countMobilityValue(board, oppositeColor(color));
-	positionValuesCache_[fen] = materialValue + mobilityValue;
 	return materialValue + mobilityValue;
 }
 
-int32_t MinmaxedPlayer::evaluatePosition(Board board, const Color& color, const int32_t depth) {
+int32_t MinmaxedPlayer::evaluatePosition(Board board, const Color& color, const int32_t depth, int32_t alpha, int32_t beta) {
 	if (depth == maxDepth || board.getGameOutcome() != Outcome::ongoing) {
 		return heuristicValue(board, color);
 	}
 
-	int32_t minMaxValue = INF;
+	int32_t bestValue = -INF;
 
-	if (const auto currFen = board.exportToFEN(); !table_.contains(currFen)) {
-		for (const auto& move : board.getAllLegalMoves(color)) {
-			board.makeMove(move);
-			table_[currFen].emplace_back(board.exportToFEN(), move);
-			if (
-				const int32_t maxEval = evaluatePosition(board, oppositeColor(color), depth + 1);
-				maxEval < minMaxValue
-			) {
-				minMaxValue = maxEval;
-			}
-			board.undoMove(move);
-		}
-	} else {
-		for (const auto& fen : table_.at(currFen) | std::views::keys) {
-			if (
-				const int32_t maxEval = evaluatePosition(Board(fen), oppositeColor(color), depth + 1);
-				maxEval < minMaxValue
-			) {
-				minMaxValue = maxEval;
-			}
+	for (const auto& move : board.getAllLegalMoves(color)) {
+		board.makeMove(move);
+		const int32_t currValue = -evaluatePosition(board, oppositeColor(color), depth + 1, -beta, -alpha);
+		board.undoMove(move);
+
+		bestValue = std::max(bestValue, currValue);
+		alpha = std::max(alpha, currValue);
+		if (alpha >= beta) {
+			break;
 		}
 	}
 
-	return -minMaxValue;
+	return bestValue;
 }
--- chess/src/MinmaxedPlayer.hpp
+++ chess/src/MinmaxedPlayer.hpp
@@ -2,18 +2,18 @@
 #include <cstdint>
 #include <string>
 #include <unordered_map>
+#include <unordered_set>
 #include <vector>
 
+#include "board.hpp"
 #include "player.hpp"
+#include "move.hpp"
 
 enum class Outcome;
 
 class MinmaxedPlayer final : public Player {
 public:
-	explicit MinmaxedPlayer(const Color color) : Player(color) {
-		table_.reserve(100000);
-		positionValuesCache_.reserve(100000);
-	}
+	explicit MinmaxedPlayer(const Color color) : Player(color) {}
 
 	[[nodiscard]] Move makeMove(Board board) override;
 
@@ -21,17 +21,9 @@ public:
 
 	static int32_t countMobilityValue(Board const& board, Color const& color);
 
-	int32_t heuristicValue(Board const& board, Color const& color);
+	static int32_t heuristicValue(Board const& board, Color const& color);
 
-	int32_t evaluatePosition(Board board, const Color& color, int32_t depth);
+	static int32_t evaluatePosition(Board board, const Color& color, int32_t depth, int32_t alpha, int32_t beta);
 
-	inline static int32_t maxDepth = 3;
-
-	size_t getCacheSize() const {
-		return positionValuesCache_.size();
-	}
-
-private:
-	std::unordered_map<std::string, std::vector<std::pair<std::string, Move>>> table_; // TODO: save board?
-	std::unordered_map<std::string, int32_t> positionValuesCache_;
+	inline static int32_t maxDepth = 4; // TODO: bigNumber which then get divided by numberOfMoves?
 };
